9f35f1d53e4072ea31e941f5d07e56ed0041347c
Demetrio <Demetrio.rey@gmail.com>
2017-02-28 09:49:51 -0400

    bcd7seg example

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 os.system("python ../examples/full-adder.py")
+os.system("python ../examples/bcd7seg.py")
 os.system("python ../examples/solve1-tt.py")
 os.system("python ../examples/solve2-tt.py")
 os.system("python ../examples/solve3-tt.py")
