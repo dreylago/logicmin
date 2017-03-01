68d89ccec183b2c5d2d4c2713c693254496d5853
Demetrio <Demetrio.rey@gmail.com>
2017-03-01 07:26:39 -0400

    fix home page

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='logicmin',
       version='0.1.2',
       description='Logic Minimization',
-      url='http://github.com/drey/logicmin',
+      url='http://github.com/dreylago/logicmin',
       author='Demetrio Rey',
       author_email='demetrio.rey@gmail.com',
       license='MIT',
